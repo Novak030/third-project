9fdc957adf65ccee1a5ecd6e5f12cfbb3a3e8aa9
Novak030 <>
2023-12-16 16:16:09 +0000

    add while true statement to repeat and for loop to get word length

--- run.py
+++ run.py
@@ -4,9 +4,23 @@ word_list = ["magical", "london", "berlin", "jazz", "thrilling", "invincible",]
 
 word = random.choice(word_list)
 
-char = input("guess: ")
+health = 6
 
-if char in word:
-    print("yes!")
-else:
-    print("No!")
+blank_word = []
+
+for i in range(0, len(word)):
+    blank_word.append("_")
+
+print(word)
+print(blank_word)
+
+
+
+while True:
+    char = input("guess: ")
+
+    if char in word:
+        print("yes!")
+    else:
+        print("No!")
+health -= 1  # health = health -1
